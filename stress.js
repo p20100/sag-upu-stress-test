d4760e19cc3709690fcbe27599d7dd4c19fa1c0c
Vincent RUDEAU <vrudeau@ideev-solutions.fr>
2025-10-16 20:56:44 +0200

    clean

--- stress.js
+++ stress.js
@@ -6,10 +6,10 @@ import addresses from './data.js';
 const respTime = new Trend('resp_time_ms', true); // garde chaque valeur (for trend export)
 
 // const host = __ENV.HOST || 'http://localhost:1407';
-const host = 'http://172.17.0.2:8080';
+// const host = 'http://172.17.0.2:8080';
 // const host =  'http://localhost:8080';
 // const host =  'http://127.0.0.1:8080';
-// const host = 'https://dev.icr-api.sortandgroup.fr';
+const host = 'https://dev.icr-api.sortandgroup.fr';
 
 const url = `${host}/api/upu/_msearch`;
 
@@ -44,7 +44,7 @@ export const options = {
       stages: [
         { target: 10, duration: '2m' },  // 10 rps
         { target: 10, duration: '3m' },  // palier
-        { target: 50, duration: '2m' },  // montée
+        { target: 50, duration: '2m' },  // montée  
         { target: 50, duration: '3m' },  // palier
         { target: 200, duration: '2m' }, // montée
         { target: 200, duration: '5m' }, // palier
